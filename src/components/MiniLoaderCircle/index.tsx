f1e3c0d5075e65d4df916beb24d6d83887c469fc
angelo <angelop_22@hotmail.com>
2020-03-17 22:33:56 -0500

    solution fix gif

--- src/components/MiniLoaderCircle/index.tsx
+++ src/components/MiniLoaderCircle/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
 import { Wrapper, GifImage } from './style';
-import loader from '../../static/miniloading.gif';
+// import loader from '../../static/miniloading.gif';
 
 interface IMiniLoader {
   style?: object;
@@ -10,7 +10,8 @@ interface IMiniLoader {
 
 const MiniLoader: React.FC<IMiniLoader> = props => {
   const { style } = props;
-  return <Wrapper>{<GifImage style={style} src={loader} />}</Wrapper>;
+  // return <Wrapper>{<GifImage style={style} src={loader} />}</Wrapper>;
+  return <Wrapper>{'Cargando...'}</Wrapper>;
 };
 
 MiniLoader.propTypes = {
