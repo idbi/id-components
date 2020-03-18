e3b2e8f8e3fffaa4de5d6403031b4b1b57a8cd49
angelo <angelop_22@hotmail.com>
2020-03-17 20:23:00 -0500

    fixs

--- src/components/MiniLoaderCircle/index.tsx
+++ src/components/MiniLoaderCircle/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
 import { Wrapper, GifImage } from './style';
-import * as loader from '../../static/miniloading.gif';
+import loader from '../../static/miniloading.gif';
 
 interface IMiniLoader {
   style?: object;
